d9a03cae9d0866d1b957675905e975424ddb6053
Ethan Fraser <ejpfraser@gmail.com>
2019-11-25 15:18:43 +1300

    fixed up to be stable

--- redditsavesorganiser/routes.py
+++ redditsavesorganiser/routes.py
@@ -1,6 +1,5 @@
 from . import app, reddit
 from .authorize import getauthurl, authorize
-from .sorting import *
 from flask import Flask, request, render_template, redirect, url_for
 import praw
 
@@ -9,7 +8,6 @@ def index():
 	if not reddit.user.me():
 		authurl = getauthurl()
 		app.jinja_env.globals.update(authurl=authurl)
-	app.jinja_env.globals.update(sort_by_subreddit=sort_by_subreddit)
 	return render_template("index.html")
 
 
